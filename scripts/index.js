c70bce13b0685a8365f8769e4cd52ed4901bfe78
Станислав Казаринов <skazarinov@mail.ru>
2024-07-30 11:47:39 +0300

    Первая редакция

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
-// @todo: Темплейт карточки
+const cardContainer = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// Функция создания карточки
+const newCard = (content, deleteCallback) => {
+  // клонируем содержимое тега template
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  // наполняем содержимым
+  cardElement.querySelector('.card__image').src = content.link;
+  cardElement.querySelector('.card__title').textContent = content.name;
+  cardElement.querySelector('.card__delete-button').addEventListener(
+    'click', () => {deleteCallback(cardElement)}
+  );
+  // возвращаем подготовленный к выводу элемент карточки
+  return cardElement;
+}
 
-// @todo: Функция создания карточки
+// Функция удаления карточки
+const deleteCard = deletedItem => {
+  deletedItem.remove();
+}
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+// Вывод карточек на страницу
+initialCards.forEach((cardItem) => {
+  cardContainer.append(newCard(cardItem, deleteCard));
+});
